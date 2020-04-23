7d7594af7ef798275619912d931447fc9b465460
xiaoxiaoyanz <15922897725@139.com>
2020-04-23 10:25:09 +0800

    cc

--- src/com/wucc/algorithm/CountSortAd.java
+++ src/com/wucc/algorithm/CountSortAd.java
@@ -24,224 +24,36 @@ public class CountSortAd {
 
 //2.创建统计数组并统计对应元素个数
 
-        int
-                []
-                countArray
-                =
+        int[] countArray = new int[d + 1];
 
-                new
-
-                        int
-                        [
-                        d
-                                +
-                                1
-                        ];
-
-        for
-        (
-                int
-                i
-                =
-                0
-                ;
-                i
-                        <
-                        array
-                                .
-                                length
-                ;
-                i
-                        ++)
-
-        {
-            countArray
-                    [
-                    array
-                            [
-                            i
-                            ]-
-                            min
-                    ]++;
+        for (int i = 0; i < array.length; i++) {
+            countArray[array[i]- min]++;
 
         }
 
 //3.统计数组做变形，后面的元素等于前面的元素之和
 
-        int
-                sum
-                =
-
-                0
-                ;
-
-        for
-        (
-                int
-                i
-                =
-                0
-                ;
-                i
-                        <
-                        countArray
-                                .
-                                length
-                ;
-                i
-                        ++)
-
-        {
-            sum
-                    +=
-                    countArray
-                            [
-                            i
-                            ];
-            countArray
-                    [
-                    i
-                    ]
-
-                    =
-                    sum
-            ;
-
+        int sum = 0;
+        for (int i = 0; i < countArray.length; i++) {
+            sum += countArray[i];
+            countArray[i] = sum;
         }
 
 //4.倒序遍历原始数列，从统计数组找到正确位置，输出到结果数组
 
-        int
-                []
-                sortedArray
-                =
-
-                new
-
-                        int
-                        [
-                        array
-                                .
-                                length
-                        ];
-
-        for
-        (
-                int
-                i
-                =
-                array
-                        .
-                        length
-                        -
-                        1
-                ;
-                i
-                        >=
-                        0
-                ;
-                i
-                        --)
-
-        {
-            sortedArray
-                    [
-                    countArray
-                            [
-                            array
-                                    [
-                                    i
-                                    ]-
-                                    min
-                            ]-
-                            1
-                    ]=
-                    array
-                            [
-                            i
-                            ];
-            countArray
-                    [
-                    array
-                            [
-                            i
-                            ]-
-                            min
-                    ]--;
-
+        int[] sortedArray = new int[array.length];
+        for (int i = array.length - 1; i >= 0; i--) {
+            sortedArray[countArray[array[i]- min]- 1]= array[i];
+            countArray[array[i]- min]--;
         }
 
-        return
-                sortedArray
-                ;
+        return sortedArray;
     }
-    public
+    public static void main(String[] args) {
 
-    static
-
-    void
-    main
-            (
-                    String
-                            []
-                            args
-            )
-
-    {
-
-        int
-                []
-                array
-                =
-
-                new
-
-                        int
-                        []
-
-                        {
-                                95
-                                ,
-                                94
-                                ,
-                                91
-                                ,
-                                98
-                                ,
-                                99
-                                ,
-                                90
-                                ,
-                                99
-                                ,
-                                93
-                                ,
-                                91
-                                ,
-                                92
-                        };
-
-        int
-                []
-                sortedArray
-                =
-                countSort
-                        (
-                                array
-                        );
-
-        System
-                .
-                        out
-                .
-                        println
-                                (
-                                        Arrays
-                                                .
-                                                        toString
-                                                                (
-                                                                        sortedArray
-                                                                ));
+        int[] array = new int[]{95, 94, 91, 98, 99, 90, 99, 93, 91, 92};
+        int[] sortedArray = countSort(array);
+        System.out.println(Arrays.toString(sortedArray));
     }
 
 }
